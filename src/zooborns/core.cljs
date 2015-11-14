cbe4c0f652d91843287111adff41e2aef308db15
Jearvon Dharrie <j.dharrie@gmail.com>
2015-11-14 11:38:04 -0500

    Render list with stubbed posts

--- src/zooborns/core.cljs
+++ src/zooborns/core.cljs
@@ -4,6 +4,14 @@
 (ns zooborns.core
   (:require [om.next :as om :refer-macros [defui]]))
 
+(def styles
+  {:container {:flex 1 :flexDirection "row" :justifyContent "center" :alignItems "center" :backgroundColor "#F5FCFF"}
+   :list-view {:paddingTop 20 :backgroundColor "#F5FCFF"}
+   :rightContainer {:flex 1}
+   :thumbnail {:backgroundColor "#3b3b39" :height 81 :width 53}
+   :title {:fontSize 20 :marginBottom 8 :textAlign "center"}
+   :year {:textAlign "center"}})
+
 ;; Reset js/React back as the form above loads in an different React
 (set! js/React (js/require "react-native/Libraries/react-native/react-native.js"))
 
@@ -15,19 +23,52 @@
 (defn text [opts & children]
   (apply js/React.createElement js/React.Text (clj->js opts) children))
 
+(defn navigator-ios [opts]
+  (js/React.createElement js/React.NavigatorIOS (clj->js opts)))
+
+(defn list-view [opts & children]
+  (js/React.createElement js/React.ListView (clj->js opts) children))
+
+(defn row-has-changed [x y]
+  (let [row-1 (js->clj x :keywordize-keys true) row-2 (js->clj y :keywordize-keys true)]
+       (not= row-1 row-2)))
+
 
 ;; Set up our Om UI
-(defonce app-state (atom {:app/msg "Welcome to zooborns"}))
+(defonce app-state (atom {:app/msg "Welcome to zooborns"
+                          :dataSource (js/React.ListView.DataSource. (clj->js {:rowHasChanged row-has-changed}))
+                          :posts [{:title "First post"}
+                                  {:title "Second post"}
+                                  {:title "Third post"}
+                                  {:title "Fourth post"}
+                                  {:title "Fifth post"}]}))
+
+(defn render-post [p]
+  (let [post (js->clj p :keywordize-keys true)]
+    (view {:style {:flexDirection "row" :paddingTop 10}}
+          (text nil (:title post)))))
+
+(def posts
+  (:posts @app-state))
+
+(defui RecentPostsComponent
+  Object
+  (render
+   [this]
+   (list-view {:dataSource (.cloneWithRows (:dataSource @app-state) (clj->js posts))
+               :renderRow render-post
+               :style (:list-view styles)})))
 
 (defui WidgetComponent
   static om/IQuery
-  (query [this]
-         '[:app/msg])
+  (query
+   [this]
+   '[:app/msg])
   Object
-  (render [this]
-          (let [{:keys [app/msg]} (om/props this)]
-            (view {:style {:flexDirection "column" :margin 40}}
-                  (text nil msg)))))
+  (render
+   [this]
+   (navigator-ios {:style {:flex 1}
+                   :initialRoute {:component RecentPostsComponent :title "ZooBorns"}})))
 
 ;; om.next parser
 (defmulti read om/dispatch)
